7df7127559978532aca2fbb9eaecd4670625587a
tanvirulislam <tanvirul.cse.diu@gmail.com>
2024-04-22 15:24:45 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/video_test_media_kit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_test_media_kit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_macros.h
@@ -0,0 +1,6 @@
+#ifndef MY_MACROS_H
+#define MY_MACROS_H
+
+#define -D-pthread 1
+
+#endif // MY_MACROS_H
